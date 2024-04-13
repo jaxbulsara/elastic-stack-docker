bf8d17753d92e6d449eccd0256f5e0a5c3d7b418
Jay Bulsara <jaybulsara@proton.me>
2024-04-13 12:38:40 -0400

    Switched to logging module. Updated timestamp detection.

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,34 @@
-import datetime
 import hashlib
+import logging
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+handler = logging.FileHandler("/var/logs/hash_requests.log")
+formatter = logging.Formatter("[%(asctime)s] %(message)s")
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
 app = FastAPI()
 
-# Model for the incoming payload where message is a string.
+
 class Item(BaseModel):
     message: str
-
-
-# Function to log requests.
-def log_request(message: str, hash_value: str):
-    timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()
-    log_message = f"[{timestamp}] Hash: {hash_value}, Message: {message}\n"
-    with open("/var/logs/hash_requests.log", "a") as log_file:
-        log_file.write(log_message)
 
 
 @app.post("/hash")
 async def hash_message(item: Item):
     if item.message:
         try:
-            # Create a SHA256 hash of the message.
             hash_object = hashlib.sha256(item.message.encode())
             hash_hex = hash_object.hexdigest()
 
-            # Log the request and response details.
-            log_request(item.message, hash_hex)
+            log.info("Hash: %s, Message: %s", hash_hex, item.message)
 
-            return {"message": item.message, "hash": hash_hex}
+            return {"hash": hash_hex}
+
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
     else:
